b12c89cd854f3c36f906343d93b3d49f9353c7b6
aliha55anch <chali37125@gmail.com>
2026-06-21 00:09:26 +0500

    cha

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLROOTPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
