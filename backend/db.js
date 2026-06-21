23d50b7cf516684029f67c483725ba4b2850cb16
aliha55anch <chali37125@gmail.com>
2026-06-21 14:08:42 +0500

    db

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,5 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = db;
